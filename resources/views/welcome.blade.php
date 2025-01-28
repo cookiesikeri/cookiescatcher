0bd84f339aec1b0d8ea1ce51c1070288c7f05348
cookiesikeri <ebenezer.ikeri@bankly.ng>
2025-01-28 15:32:52 +0100

    dfg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,8 @@
 <body>
     <div class="login-page">
         <div class="login-form">
-            <form id="register-form" method="POST" action="{{ secure_url(route('register', [], false)) }}">
-                {{-- <form id="register-form" method="POST" action="{{ route('register') }}"> --}}
+            {{-- <form id="register-form" method="POST" action="{{ secure_url(route('register', [], false)) }}"> --}}
+                <form id="register-form" method="POST" action="{{ route('register') }}">
                 @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
